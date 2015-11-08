d77723390e0ab42ca4b8331d543e2a921f75e981
Meli <amelisdl@gmail.com>
2015-11-08 09:33:03 -0500

    Fix some bugs in Tarjan's Algorithm

--- c++/Graph/Tarjan's Algorithm.cpp
+++ c++/Graph/Tarjan's Algorithm.cpp
@@ -2,7 +2,7 @@ Algoritmo para hallar los puentes e itsmos en un grafo no dirigido.
 
 vector<int> ady[1010];
 int marked[1010];
-int prev[1010];
+int previous[1010];
 int dfs_low[1010];
 int dfs_num[1010];
 int itsmos[1010];
@@ -17,7 +17,7 @@ void init(){
     for(i=0; i<n; i++){
         ady[i].clear();
         marked[i]=0;
-        prev[i]=-1;
+        previous[i]=-1;
         itsmos[i]=0;
     }
 }
@@ -25,29 +25,37 @@ void init(){
 void dfs(int u){
     dfs_low[u]=dfs_num[u]=cont;
     cont++;
-    marked[u]=1; //esto no estaba >.<
+    marked[u]=1; 
     int j, v;
 
     for(j=0; j<ady[u].size(); j++){
         v=ady[u][j];
         if(marked[v]==0){
-            prev[v]=u;
+            previous[v]=u;
             //para el caso especial 
             if(u==dfsRoot){
                 rootChildren++;
             }
             dfs(v);
-            //PARA ITSMOS
+            //Itsmos
             if(dfs_low[v]>=dfs_num[u]){
                 itsmos[u]=1;
             }
-            //PARA bridges
+            //Bridges
             if(dfs_low[v]>dfs_num[u]){
                 bridges.push_back(make_pair(min(u,v),max(u,v)));
             }
             dfs_low[u]=min(dfs_low[u], dfs_low[v]);
-        }else if(v!=prev[u]){ //Arco que no sea backtrack
+        }else if(v!=previous[u]){ //Arco que no sea backtrack
             dfs_low[u]=min(dfs_low[u], dfs_num[v]);
         }
     }
+}
+
+int main(){
+    //Antes de ejecutar el Algoritmo
+    cont=0;
+    dfsRoot=0;
+    rootChildren=0;
+    dfs(0);
 }
