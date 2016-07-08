e979ea019b277c9d0f93f9043fcd3690a1710f15
Gerson Lázaro <GersonLazaro@GersonLazaro.com>
2016-07-08 17:32:04 -0500

    Actualizada exponenciación binaria en java y C++, añadido miller rabin en java y actualizado en c++ #32

--- c++/Math/Binary Exponentiation.cpp
+++ c++/Math/Binary Exponentiation.cpp
@@ -1,15 +1,15 @@
-Realiza a^b y retorna el resultado módulo c. Si se elimina el módulo c, debe tenerse precaución para no exceder el límite
+Realiza a^b y retorna el resultado módulo c
 
 
-int binaryExponentiation(int a, int b, int c){
+long long binaryExponentiation(long long a, long long b, long long c){
     if (b == 0){
     	return 1;
     } 
     if (b % 2 == 0){
-        int temp = binaryExponentiation(a,b/2, c);
-        return ((long long)(temp) * temp) % c;
+        long long temp = binaryExponentiation(a,b/2, c);
+        return (temp * temp) % c;
     }else{
-        int temp = binaryExponentiation(a, b-1, c);
-        return ((long long)(temp) * a) % c;
+        long long temp = binaryExponentiation(a, b-1, c);
+        return (temp * a) % c;
     }
 }
--- java/Math/Miller-Rabin.java
+++ java/Math/Miller-Rabin.java
@@ -1,10 +1,8 @@
 La función de Miller-Rabin determina si un número dado es o no un número primo. IMPORTANTE: Debe utilizarse el método binaryExponentiation.
 
-#include <cstdlib>
-
-long long mulmod(long long a, long long b, long long mod){
-    long long x = 0;
-    long long y = a % mod;
+public static long mulmod(long a, long b, long mod){
+  	long x = 0;
+    long y = a % mod;
     while (b > 0){
         if (b % 2 == 1){    
             x = (x + y) % mod;
@@ -14,23 +12,22 @@ long long mulmod(long long a, long long b, long long mod){
     }
     return x % mod;
 }
- 
 
-bool miller(long long  p){
+public static boolean miller(long p){
     if (p < 2){
         return false;
     }
     if (p != 2 && p % 2==0){
         return false;
     }
-    long long s = p - 1;
+    long s = p - 1;
     while (s % 2 == 0){
         s /= 2;
     }
     for (int i = 0; i < 5; i++){
-        long long a = rand() % (p - 1) + 1;
-        long long temp = s;
-        long long mod = binaryExponentiation(a, temp, p);
+        long a = (long)(Math.random() * 100000000) % (p - 1) + 1;
+        long temp = s;
+        long mod = binaryExponentiation(a, temp, p);
         while (temp != p - 1 && mod != 1 && mod != p - 1){
             mod = mulmod(mod, mod, p);
             temp *= 2;
