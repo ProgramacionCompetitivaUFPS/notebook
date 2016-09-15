42e0bda4a1fd4699e1dbe3f4ceaafcf2cae4e465
Gerson Lázaro <GersonLazaro@GersonLazaro.com>
2016-09-15 00:05:43 -0500

    Actualización de comentarios RMQ

--- c++/Data Structures/RMQ.cpp
+++ c++/Data Structures/RMQ.cpp
@@ -1,13 +1,16 @@
-class SegmentTree {         // the segment tree is stored like a heap array
-private: vector<int> st, A;            // recall that vector<int> is: typedef vector<int> vector<int>;
+Range minimum query. Recibe como parametro en el constructor un array de valores. Las consultas se realizan con el método rmq(indice_inicio, indice_final) y pueden actualizarse los valores con update_point(indice, nuevo_valor)
+
+
+class SegmentTree {         
+private: vector<int> st, A;            
   int n;
-  int left (int p) { return p << 1; }     // same as binary heap operations
+  int left (int p) { return p << 1; }     
   int right(int p) { return (p << 1) + 1; }
 
-  void build(int p, int L, int R) {                           // O(n log n)
-    if (L == R)                            // as L == R, either one is fine
-      st[p] = L;                                         // store the index
-    else {                                // recursively compute the values
+  void build(int p, int L, int R) { 
+    if (L == R) 
+      st[p] = L;
+    else { 
       build(left(p) , L, (L + R) / 2);
       build(right(p), (L + R) / 2 + 1, R);
       int p1 = st[left(p)], p2 = st[right(p)];
@@ -15,51 +18,39 @@ private: vector<int> st, A;            // recall that vector<int> is: typedef ve
     } 
   }
 
-  int rmq(int p, int L, int R, int i, int j) {                  // O(log n)
-    if (i >  R || j <  L) return -1; // current segment outside query range
-    if (L >= i && R <= j) return st[p];               // inside query range
-
-     // compute the min position in the left and right part of the interval
+  int rmq(int p, int L, int R, int i, int j) { 
+    if (i >  R || j <  L) return -1; 
+    if (L >= i && R <= j) return st[p]; 
     int p1 = rmq(left(p) , L, (L+R) / 2, i, j);
     int p2 = rmq(right(p), (L+R) / 2 + 1, R, i, j);
 
-    if (p1 == -1) return p2;   // if we try to access segment outside query
-    if (p2 == -1) return p1;                               // same as above
-    return (A[p1] <= A[p2]) ? p1 : p2; }          // as as in build routine
+    if (p1 == -1) return p2;   
+    if (p2 == -1) return p1; 
+    return (A[p1] <= A[p2]) ? p1 : p2; } 
 
   int update_point(int p, int L, int R, int idx, int new_value) {
     int i = idx, j = idx;
 
-    // if the current interval does not intersect 
-    // the update interval, return this st node value!
     if (i > R || j < L)
       return st[p];
-
-    // if the current interval is included in the update range,
-    // update that st[node]
     if (L == i && R == j) {
-      A[i] = new_value; // update the underlying array
-      return st[p] = L; // this index
+      A[i] = new_value; 
+      return st[p] = L; 
     }
-
-    // compute the minimum pition in the 
-    // left and right part of the interval
     int p1, p2;
     p1 = update_point(left(p) , L, (L + R) / 2, idx, new_value);
     p2 = update_point(right(p), (L + R) / 2 + 1, R, idx, new_value);
-
-    // return the pition where the overall minimum is
     return st[p] = (A[p1] <= A[p2]) ? p1 : p2;
   }
 
 public:
   SegmentTree(const vector<int> &_A) {
-    A = _A; n = (int)A.size();              // copy content for local usage
-    st.assign(4 * n, 0);            // create large enough vector of zeroes
-    build(1, 0, n - 1);                                  // recursive build
+    A = _A; n = (int)A.size();
+    st.assign(4 * n, 0);   
+    build(1, 0, n - 1);
   }
 
-  int rmq(int i, int j) { return rmq(1, 0, n - 1, i, j); }   // overloading
+  int rmq(int i, int j) { return rmq(1, 0, n - 1, i, j); }
 
   int update_point(int idx, int new_value) {
     return update_point(1, 0, n - 1, idx, new_value); }
