1f4444e922a8f39959f059eeae1dc375fd84b03a
Angie Melissa Delgado <amelisdl@gmail.com>
2017-06-21 11:06:51 -0500

    Resolve issues #42, #31, #43

--- java/Graphs/DFS.java
+++ java/Graphs/DFS.java
@@ -1,11 +1,9 @@
 Algoritmo de búsqueda en profundidad para grafos. Parte de un nodo inicial s visita a todos sus vecinos. DFS puede ser usado para contar la cantidad de componentes conexas en un grafo y puede ser modificado para que retorne información de los nodos dependiendo del problema. Permite hallar ciclos en un grafo.
 
-import java.util.ArrayList;
-
 static int v, e; //vertices, arcos
-static int MAX=100005; 
-static ArrayList<Integer> ady[] = new ArrayList[MAX];
-static boolean marked[] = new boolean[MAX];
+static int MAX=100005; //Cantidad máxima de nodos del grafo
+static ArrayList<Integer> ady[] = new ArrayList[MAX]; //Lista de adyacencia
+static boolean marked[] = new boolean[MAX]; //Estructura auxiliar para marcar los grafos visitados
 
 //Limpia las estrucuturas de datos
 static void init(){
--- java/Graphs/Topological Sort.java
+++ java/Graphs/Topological Sort.java
@@ -1,24 +1,24 @@
 Dado un grafo acíclico y dirigido, ordena los nodos linealmente de tal manera que si existe una arista entre los nodos u y v entonces u aparece antes que v.
 Este ordenamiento es una manera de poner todos los nodos en una línea recta de tal manera que las aristas vayan de izquierda a derecha.
 
-int v; //Cantidad de nodos del grafo
-const int MAX=100005; //Cantidad máxima de nodos del grafo
-vector<int> topoSort; //Ordenamiento topologico del grafo
-vector<int> ady[MAX]; //Lista de adyacencia
-bool marked[MAX]; //Estructura auxiliar para marcar los grafos visitados
+static int v, e; //vertices, arcos
+static int MAX=100005; //Cantidad máxima de nodos del grafo
+static ArrayList<Integer> ady[] = new ArrayList[MAX]; //Lista de adyacencia
+static ArrayList<Integer> topoSort; //Lista de adyacencia
+static boolean marked[] = new boolean[MAX]; //Estructura auxiliar para marcar los grafos visitados
 
 //Recibe un nodo inicial u
-void dfs( int u ){
+static void dfs( int u ){
 	int i, v;
 	marked[u] = 1;
 	for( i = 0; i < ady[u].size(); i++){
-		v = ady[u][i];
+		v = ady[u].get(i);
 		if( !marked[v] ) dfs(v);
 	}
-	topoSort.push_back(u);
+	topoSort.add(u);
 }
 
-int main(){
+public static void main( String args[]){
 	for(i=0; i<v; i++){
 		if( !marked[i] )	dfs(i)
 	}
