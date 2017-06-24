588ca7a22e60dd1c54d9e5a9d39e6cf5932e8ae5
GersonLazaro <gersonlazaro@gersonlazaro.com>
2017-06-23 22:14:51 -0500

    Agregado LCS. fix #34

---
+++ c++/Dynamic Programming/Longest Common Subsequence.cpp
@@ -0,0 +1,19 @@
+Dados dos Strings, encuentra el largo de la subsecuencia en común mas larga entre ellas.
+
+const int M_MAX = 20; // Máximo size del String 1
+const int N_MAX = 20; // Máximo size del String 2 
+int m, n; // Size de Strings 1 y 2
+string X; // String 1
+string Y; // String 2
+int memo[M_MAX + 1][N_MAX + 1];
+
+int lcs (int m, int n) {
+  for (int i = 0; i <= m; i++) {
+    for (int j = 0; j <= n; j++) {
+      if (i == 0 || j == 0) memo[i][j] = 0;
+      else if (X[i - 1] == Y[j - 1]) memo[i][j] = memo[i - 1][j - 1] + 1;
+      else memo[i][j] = max(memo[i - 1][j], memo[i][j - 1]);
+    }
+  }
+  return memo[m][n];
+}
