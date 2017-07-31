9c7378661184e6b8a5055be4abdc6a2cd9191e44
Gerson Lázaro <GersonLazaro@GersonLazaro.com>
2017-07-30 21:40:52 -0500

    Actualizado lis en java, fix #62

--- java/Dynamic Programming/Longest increasing subsequence.java
+++ java/Dynamic Programming/Longest increasing subsequence.java
@@ -1,22 +1,27 @@
-Halla la longitud de la subsecuencia creciente mas larga. MAX debe definirse en el tamaño  limite del array, n es el tamaño del array. Puede aplicarse también sobre strings, cambiando el parametro int s[] por string s. Si debe ser estrictamente creciente, cambiar el <= de s[j] <= s[i] por <
+Halla la longitud de la subsecuencia creciente mas larga. MAX debe definirse en el tamaño limite del array, n es el tamaño del array. Si debe admitir valores repetidos, cambiar el < de I[mid] < values[i] por <=
 
-static int MAX = 1005;
-static int memo[] = new int[MAX];
+static int inf = 2000000000;
+static int MAX = 100000; 
+static int n;
+static int values[] = new int[MAX + 5];
+static int L[] = new int[MAX + 5]; 
+static int I[] = new int[MAX + 5]; 
 
-static int longestIncreasingSubsequence (int s[]) {
-	int n = s.length;
-	memo[0] = 1;
-	int output = 1;
-	for (int i = 1; i < n; i++){
-		memo[i] = 1;
-		for (int j = 0; j < i; j++){
-			if (s[j] <= s[i] && memo[i] < memo[j] + 1){
-				memo[i] =  memo[j] + 1;
-			} 
-		}
-		if(memo[i] > output){
-			output = memo[i];
-		}
+static int lis() { 
+ 	int i, low, high, mid;
+ 	I[0] = -inf; 
+ 	for (i = 1; i <= n; i++) I[i] = inf;
+  	int ans = 0;
+ 	for(i = 0; i < n; i++) {
+		low = mid = 0;
+ 		high = ans;
+ 		while(low <= high) {
+ 			mid = (low + high) / 2;
+ 			if(I[mid] < values[i]) low = mid + 1;
+ 			else high = mid - 1;
+ 		} 
+ 		I[low] = values[i];
+ 		if(ans < low) ans = low;
 	}
-	return output;
+ 	return ans;
 }
