cc280305ce52220feb14b7c5b74bcae2530652db
Gerson Lázaro <GersonLazaro@GersonLazaro.com>
2017-07-30 21:34:00 -0500

    Actualizando LIS en C++ para cambiar solución n^2 por nlogn

--- c++/Dynamic Programming/Longest Increasing Subsequence.cpp
+++ c++/Dynamic Programming/Longest Increasing Subsequence.cpp
@@ -1,19 +1,27 @@
-Halla la longitud de la subsecuencia creciente mas larga. MAX debe definirse en el tamaño  limite del array, n es el tamaño del array. Puede aplicarse también sobre strings, cambiando el parametro int s[] por string s. Si debe ser estrictamente creciente, cambiar el <= de s[j] <= s[i] por <
+Halla la longitud de la subsecuencia creciente mas larga. MAX debe definirse en el tamaño  limite del array, n es el tamaño del array. Si se admiten valores repetidos, cambiar el < de I[mid] <= values[i] por <=
 
-const int MAX = 1005;
-int memo[MAX];
+const int inf = 2000000000;
+const int MAX = 100000; //Máximo número de valores en la secuencia
+int n; //Tamaño de la secuencia
+int values[MAX + 5]; //Secuencia de valores
+int L[MAX + 5]; 
+int I[MAX + 5]; 
 
-int longestIncreasingSubsequence(int s[], int n){
-	memo[0] = 1;
-	int output = 1;
-	for (int i = 1; i < n; i++){
-		memo[i] = 1;
-		for (int j = 0; j < i; j++){
-			if (s[j] <= s[i] && memo[i] < memo[j] + 1){
-				memo[i] =  memo[j] + 1;
-			} 
-		}
-		output = max(output, memo[i]);
+int lis() { 
+ 	int i, low, high, mid;
+ 	I[0] = -inf; 
+ 	for (i = 1; i <= n; i++) I[i] = inf;
+ 	int ans = 0;
+ 	for(i = 0; i < n; i++) {
+ 		low = mid = 0;
+ 		high = ans;
+ 		while(low <= high) {
+ 			mid = (low + high) / 2;
+ 			if(I[mid] < values[i]) low = mid + 1;
+ 			else high = mid - 1;
+ 		} 
+ 		I[low] = values[i];
+ 		if(ans < low) ans = low;
 	}
-	return output;
+ 	return ans;
 }
