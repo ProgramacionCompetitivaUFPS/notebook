c427c57ff8d20c28fa558e9c0e2413da0e55ed8f
karen2427 <38541592+karen2427@users.noreply.github.com>
2018-10-08 15:25:27 -0500

    Update Z-Function.java

--- java/String/Z-Function.java
+++ java/String/Z-Function.java
@@ -1,4 +1,5 @@
-Este algoritmo encuentra todas las apariciones de un patrón en un texto en tiempo lineal. Sea la longitud del texto n y del patrón m, entonces el tiempo total tomado es O (m + n) con complejidad de espacio lineal.
+Este algoritmo encuentra todas las apariciones de un patrón en un texto en tiempo lineal.
+    Sea la longitud del texto n y del patrón m, entonces el tiempo total tomado es O (m + n) con complejidad de espacio lineal.
 
 static int[] zf(String ss) {
     StringBuilder s = new StringBuilder(ss);
