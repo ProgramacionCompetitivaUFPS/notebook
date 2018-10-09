273a6caa26ed3cd902c714344520461b9d86dd8c
William Moreno <32917746+WearyJunger@users.noreply.github.com>
2018-10-09 17:23:23 -0500

    Update Tarjan.java
    Inicialización de las estructuras necesarias para la ejecución del algoritmo.

--- java/Graphs/Tarjan.java
+++ java/Graphs/Tarjan.java
@@ -1,7 +1,7 @@
 Algoritmo para hallar componentes fuertemente conexas(SCC) en grafos dirigidos.
 SE DEBEN LIMPIAR LAS ESTRUCTURAS DE DATOS ANTES DE UTILIZARSE
 
-int v, e; 
+static int v;
 static int n = 5000; // Máxima cantidad de nodos
 static int dfs_low[] = new int[n];
 static int dfs_num[] = new int[n];
@@ -10,24 +10,12 @@ static Stack<Integer> s;
 static int dfsCont, cantSCC;
 static ArrayList<Integer> ady[] = new ArrayList[n];
 	
-public static void main (String[] args){
-    for( int i = 0; i < v; i++ ){ //Por si el grafo no es conexo
-        if( dfs_num[i] == -1 ){
-            dfsCont = 0;
-            s = new Stack<Integer>();
-            tarjanSCC(i);
-        }
-    }
-}
-	
 public static void tarjanSCC( int u ){
 	dfs_low[u] = dfs_num[u] = dfsCont;
 	dfsCont++;
 	s.push(u);
-	marked[u] = true;
-		
-	int j, v;
-		
+	marked[u] = true;		
+	int j, v;		
 	for( j = 0; j < ady[u].size(); j++ ){
 		v = ady[u].get( j );
 			
@@ -41,20 +29,37 @@ public static void tarjanSCC( int u ){
 	}
 		
 	if( dfs_low[u] == dfs_num[u] ){
-		cantSCC++;
-        /* ********************************************************* */
-        /* Esta seccion se usa para imprimir las componentes conexas */
-		System.out.println("COMPONENTE CONEXA #" + cantSCC );
+		cantSCC++;	
+        /* Esta seccion comentada se usa para imprimir las componentes conexas */		
+		/* System.out.println("COMPONENTE CONEXA #" + cantSCC );
 		while( !s.empty() ){
 			v = s.peek();
 			s.pop();
 			marked[v] = false;
 			System.out.println(v);
 			if( u == v ) break;
-		}
-        /* ********************************************************** */
+		} */
 	}
-		
 }
+
+	
+public static void main (String[] args){
+	
+	 for (int i = 0; i <n; i++) { //inicializa las estructuras necesarias para la ejecucion del algoritmo.
+                ady[i] = new ArrayList<Integer>();
+                dfs_low[i] = 0;
+                dfs_num[i] = -1;
+		 marked[i] = false;
+            }
+	
+    	for( int i = 0; i < v; i++ ){ //Por si el grafo no es conexo
+        if( dfs_num[i] == -1 ){
+            dfsCont = 0;
+            s = new Stack<Integer>();
+            tarjanSCC(i);
+        }
+    }
+}
+
 
 
