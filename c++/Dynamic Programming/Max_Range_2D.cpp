9e6ffc2838406969c129a09a2091e3668a89bff4
carlosfernandoufps <42192848+carlosfernandoufps@users.noreply.github.com>
2018-12-15 15:03:49 -0500

    Update Max_Range_2D.cpp

--- c++/Dynamic Programming/Max_Range_2D.cpp
+++ c++/Dynamic Programming/Max_Range_2D.cpp
@@ -2,13 +2,13 @@
 
 //Cambiar infinito por el mínimo valor posible
 int INF = -100000007;
-int n, m;
+int n, m; //filas y columnas
 const int MAX_N = 105, MAX_M = 105;
 int values[MAX_N][MAX_M];
 
 int max_range_sum2D(){
 	for(int i=0; i<n;i++){
-		for(int j=0; j<n; j++){
+		for(int j=0; j<m; j++){
 			cin>>values[i][j];
 			if(i>0) values[i][j] += values[i-1][j];
 			if(j>0) values[i][j] += values[i][j-1];
@@ -17,9 +17,9 @@ int max_range_sum2D(){
 	}
 	int max_mat = -INF;
 	for(int i=0; i<n;i++){
-		for(int j=0; j<n; j++){
+		for(int j=0; j<m; j++){
 			for(int h = i; h<n; h++){
-				for(int k  = j; k<n; k++){
+				for(int k  = j; k<m; k++){
 					int sub_mat = values[h][k];
 					if(i>0) sub_mat -= values[i-1][k];
 					if(j>0) sub_mat -= values[h][j-1];
