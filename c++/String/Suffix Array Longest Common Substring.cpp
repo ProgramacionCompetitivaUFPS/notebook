0eb9643a79bcb1acd06338f2b06bf8f1515742cb
veg8sebastian <bsebastianvg18@gmail.com>
2019-01-11 22:14:46 -0500

    update suffix

--- c++/String/Suffix Array Longest Common Substring.cpp
+++ c++/String/Suffix Array Longest Common Substring.cpp
@@ -1,7 +1,7 @@
 Busca el substring común mas largo entre dos strings. Retorna un par, con el size del substring y uno de los indices del suffix array. Debe ejecutarse previamente suffixArray() y calculateLCP()
 
 // Los substrings deben estar concatenados de la forma "string1#string2$", antes de ejecutar suffixArray() y calculateLCP()
-// m debe almacenar el size del string2.
+int m;//size del string2.
 
 pair<int, int> longestCommonSubstring() {
   int i, ind = 0, lcs = -1;
