96f4dd175c7fa8b3f418d9a5223b8babcd6655c0
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-01-15 16:25:48 -0500

    Correcciones menores: Sieve of Eratosthenes

--- java/Math/Sieve of Eratosthenes.java
+++ java/Math/Sieve of Eratosthenes.java
@@ -1,28 +1,20 @@
-Guarda en primes los números primos menores o iguales a MAX.
+Guarda en primes los números primos menores a MAX.
 isPrime() retorna si p es o no un número primo.
 
-static int MAX = 10000000;
+static int MAX = 1000000;
 static ArrayList<Integer> primes = new ArrayList<Integer>();
-static boolean sieve[] = new boolean[MAX/2];
+static boolean marked[] = new boolean[MAX/2];
 	
-static void calculatePrimes() {
-	sieve[0] = true;
+static void sieve() {
+	marked[0] = true;
 	primes.add(2);
-	int i;
-	for (i = 3; i*i <= MAX; i += 2) {
-		if (!sieve[i/2]) {
-			primes.add(i);
-			for (int j = i*i; j <= MAX; j += i*2) {
-				sieve[j/2] = true;
-			}
-		}
-	}
-	for(; i <= MAX; i += 2) {
-		if (!sieve[i/2]) primes.add(i);
+	for (long i = 3; i < MAX; i += 2) if (!marked[i/2]) {
+		primes.add(i);
+		for (long j = i*i; j < MAX; j += i*2) marked[j/2] = true;
 	}
 }
 
 static boolean isPrime(int p) {
-	if (p%2 == 0) return p == 2;
-	return !sieve[p/2];
+	if (p%2 != 0) return !sieve[p/2];
+	return p == 2;
 }
