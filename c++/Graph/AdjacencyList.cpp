544dbfef05635a23ccd36ed14714aaa62ed16130
karen2427 <38541592+karen2427@users.noreply.github.com>
2019-02-13 19:34:50 -0500

    Update AdjacencyList.cpp

--- c++/Graph/AdjacencyList.cpp
+++ c++/Graph/AdjacencyList.cpp
@@ -2,46 +2,16 @@
 using namespace std;
 
 int v, e; //v = cantidad de nodos, e = cantidad de aristas
-<<<<<<< HEAD
-const int MAX=100005; //Cantidad máxima de nodos del grafo
-vector<int> ady[MAX]; //lista de Adyacencia del grafo
-
-void init() {
-    int i;
-    for( i = 0; i < v; i++ ) {
-        ady[i].clear();
-=======
 const int MAX = 5000; // Maxima cantidad de nodos
 vector<int> ady[MAX]; //Lista de Adyacencia del grafo
 
 static void init(){
     for(int i = 0; i < v; i++ ){
       ady[i].clear();
->>>>>>> 614f0414ad40c6e48e127ff962300328060d43b8
     }
 }
 
 int main() {
-<<<<<<< HEAD
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int origen, destino;
-
-    //Al iniciar cada caso de prueba
-    cin>>v>>e;
-    init();
-
-    while( e > 0 ) {
-        cin>>origen>>destino;
-
-        ady[ origen ].push_back( destino );
-        ady[ destino ].push_back( origen );
-        e--;
-    }
-
-    return 0;
-=======
     int origen, destino,i;
     
     //Al iniciar cada caso de prueba
@@ -65,5 +35,4 @@ int main() {
 	cout<<"\n";
      }
      return 0;
->>>>>>> 614f0414ad40c6e48e127ff962300328060d43b8
 }
