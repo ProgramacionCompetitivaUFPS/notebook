02403aa61abc85b2d441c3cc32994615a5ed6e16
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-02-17 20:45:28 -0500

    Update IO folder and add Utilities folder

---
+++ c++/Bonus Input Output/scanf and printf.cpp
@@ -0,0 +1,15 @@
+* Lectura según el tipo de dato (Se usan las mismas para imprimir)
+
+scanf("%d", &value); //int
+scanf("%ld", &value); //long y long int
+scanf("%c", &value); //char 
+scanf("%f", &value); //float
+scanf("%lf", &value); //double
+scanf("%s", &value); //char*
+scanf("%lld", &value); //long long int
+scanf("%x", &value); //int hexadecimal
+scanf("%o", &value); //int octal
+
+* Impresión de punto flotante con d decimales (ej: d = 6)
+
+printf("%.6lf", value);
