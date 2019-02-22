dab27840b119aa5a08d5db3868780042abf55d01
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-02-22 11:08:00 -0500

    Format Linear Recurrende.cpp

--- c++/Math/Linear Recurrence.cpp
+++ c++/Math/Linear Recurrence.cpp
@@ -2,14 +2,14 @@ Calcula el fibonacci de n como la suma de los k terminos anteriores de la secuen
 IMPORTANTE: Debe agregarse Matrix Multiplication.
 
 int fib(int k, long long n) {
-	matrix F(k, 1);
-	F.m[0][0] = 1;
-	for (int i = 0; i < k-1; i++)
-		F.m[i+1][0] = 1 << i;
-	matrix T(k, k);
-	for (int i = 0; i < k; i++)
-		for (int j = 0; j < k; j++)
-			if (i == k-1 || i == j-1) T.m[i][j] = 1;
-	F = F * pow(T, n+k);
-	return F.m[0][0];
+    matrix F(k, 1);
+    F.m[0][0] = 1;
+    for (int i = 0; i < k-1; i++)
+        F.m[i+1][0] = 1 << i;
+    matrix T(k, k);
+    for (int i = 0; i < k; i++)
+        for (int j = 0; j < k; j++)
+            if (i == k-1 || i == j-1) T.m[i][j] = 1;
+    F = F * pow(T, n+k);
+    return F.m[0][0];
 }
