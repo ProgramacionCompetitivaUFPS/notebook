320c04c55d29f192b4560e44fa3864255d94f46d
Crisel Ayala <criseljazminal@ufps.edu.co>
2019-03-28 17:00:15 -0500

    Update Disjoint Set.java

--- java/Data Structures/Disjoint Set.java
+++ java/Data Structures/Disjoint Set.java
@@ -3,16 +3,15 @@ Permite determinar de manera eficiente a que conjunto pertenece un elemento,
 si dos elementos se encuentran en un mismo conjunto y unir dos conjuntos disyuntos en un uno.
 
 static int MAX = 10001; //Cantidad maxima de conjuntos disyuntos
-static int dsu[] = new int[MAX]; //Estructura de DS
-static int size[] = new int[MAX]; //Estructura para almacenar el tamano de los conjuntos
-static int rnk[] = new int[MAX]; //Estructura para optimizacion: compresion de ruta
-static int cantSets; //Cantidad de conjuntos disyuntos existentes
+static int dsu[] = new int[MAX]; //Almacena al representante de cada conjunto
+static int size[] = new int[MAX]; //Almacena eltamaño de cada conjunto
+static int numSets; //Cantidad de conjuntos disyuntos existentes
 
 /* Recibe la cantidad de conjuntos disyuntos iniciales */
 static void init(int n) {
-	cantSets = n;
+	numSets = n;
 	for(int i = 0; i < n; i++) {
-		dsu[i] = i; size[i] = 1; rnk[i] = 0;
+		dsu[i] = i; size[i] = 1;
 	}
 }
 
@@ -20,14 +19,14 @@ static int find(int i) {
 	return (dsu[i] == i) ? i : (dsu[i] = find(dsu[i]));
 }
 
-static void union(int x, int y) {
-	x = find(x); y = find(y);
-	if(x == y) return;
-	cantSets--;
-	if (rnk[x] > rnk[y]) { int z=x;x=y;y=z; };
-	dsu[x] = y;
-	size[y] += size[x];
-	rnk[y] += (rnk[x] == rnk[y] ? 1 : 0);
+static void unite(int a, int b) {
+	a = find(a); b = find(b);
+	if(a == b) return;
+	if (size[a] > size[b]){int c=a; a=b; b=c;}
+	dsu[a] = b;
+	size[b] += size[a];
+	numSets--;
 }
 
 static int sizeOf(int i) { return size[find(i)]; }
+	
