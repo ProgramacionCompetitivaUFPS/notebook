fcecf6e8b74338d15c8d5c22871e5612f7faa440
Elizabeth Ramirez <eramirezvillamizar@gmail.com>
2019-04-04 20:30:06 -0500

    updating to java class

--- java/Data Structures/Disjoint Set.java
+++ java/Data Structures/Disjoint Set.java
@@ -2,31 +2,43 @@ Estructura de datos para modelar una colección de conjuntos disyuntos.
 Permite determinar de manera eficiente a que conjunto pertenece un elemento,
 si dos elementos se encuentran en un mismo conjunto y unir dos conjuntos disyuntos en un uno.
 
-static int MAX = 10001; //Cantidad maxima de conjuntos disyuntos
-static int dsu[] = new int[MAX]; //Almacena al representante de cada conjunto
-static int size[] = new int[MAX]; //Almacena eltamaño de cada conjunto
-static int numSets; //Cantidad de conjuntos disyuntos existentes
-
-/* Recibe la cantidad de conjuntos disyuntos iniciales */
-static void init(int n) {
-	numSets = n;
-	for(int i = 0; i < n; i++) {
-		dsu[i] = i; size[i] = 1;
-	}
-}
-
-static int find(int i) {
-	return (dsu[i] == i) ? i : (dsu[i] = find(dsu[i]));
-}
-
-static void unite(int a, int b) {
-	a = find(a); b = find(b);
-	if(a == b) return;
-	if (size[a] > size[b]){int c=a; a=b; b=c;}
-	dsu[a] = b;
-	size[b] += size[a];
-	numSets--;
-}
-
-static int sizeOf(int i) { return size[find(i)]; }
-	
+static int MAX = 100005;
+    
+    class Dsu{
+        
+        int dsu[] = new int[MAX];
+        int size[] = new int[MAX];
+        int numsets;
+        
+        
+        Dsu(int n){
+            for(int i = 0; i <= n; i++){
+                dsu[i] = i;
+                size[i] = 1;
+            }
+            numsets = n;
+        }
+        
+        int find(int u){
+            return (dsu[u] == u) ? u : (dsu[u] = find(dsu[u]));
+        }
+        
+        void union(int a,int b){
+            a = find(a);
+            b = find(b);
+            if(a == b)return;
+            if(size[a] > size[b]){
+                int c = a;
+                a = b;
+                b = c;
+            }
+            dsu[a] = b;
+            size[b] += size[a];
+            numsets--;
+        }
+        
+        int sizeOf(int i) {
+            return size[find(i)]; 
+        }
+        
+    }
