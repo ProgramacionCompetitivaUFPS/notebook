cadf5d1beb76bd0e012add7005b1be1f3053e4bf
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-04-24 16:39:55 -0500

    Update Extended Euclides.java

--- java/Math/Extended Euclides.java
+++ java/Math/Extended Euclides.java
@@ -1,14 +1,16 @@
 El algoritmo de Euclides extendido retorna el gcd(a, b) y calcula los coeficientes enteros X y Y que satisfacen la ecuación: a*X + b*Y = gcd(a, b).
 
-public static long extendedEuclid(int a, int b) {
-    if (b == 0) {
-        x = 1;
-        y = 0;
-        return a;
+static int x, y;
+
+static int extendedEuclid(int a, int b) {
+    if (b == 0) { 
+    	x = 1;
+    	y = 0;
+    	return a;
     }
-    long aux = extendedEuclid(b, a % b);
+    int d = extendedEuclid(b, a % b);
     int temp = x;
     x = y;
-    y = temp - (a / b) * y;
-    return aux;
+    y = temp - ((a/b)*y);
+    return d;
 }
