33d2ad1c6835cf3c084bf86f68dda3d3c4027660
Veg8Sebastian <32554701+Veg8Sebastian@users.noreply.github.com>
2019-04-23 23:29:46 -0500

    Update String Hashing.cpp

--- c++/String/String Hashing.cpp
+++ c++/String/String Hashing.cpp
@@ -1,23 +1,32 @@
 Estructura para realizar operaciones de hashing. 
 
-int p = 265; //Número pseudo-aleatorio base del polinomio (mayor al tamaño del lenguaje)
-int MOD = 1000000009; //Número primo grande
+#define mod(a, b) (((a)%(b)+(b))%(b))
+
+ll p[] = {153, 255};
+ll MOD[] = {1000000007, 1000000009};
+ll X = 1e9 + 10;
 
 struct hashing {
-	string s;
-	vector<int> h;
-	vector<int> pot;
-	hashing(string _s) {
-		h.resize(_s.size() + 1);
-		pot.resize(_s.size() + 1);
-		s = _s; h[0] = 0; pot[0] = 1;
-		for(int i = 1; i <= s.size(); i++) {
-			h[i] = ((long long)h[i - 1] * p + s[i - 1]) % MOD;
-			pot[i] = ((long long)pot[i - 1] * p) % MOD;
-		}
-	}
-	int hashValue(int i, int j) {
-		int ans = h[j] - (long long) h[i] * pot[j - i] % MOD;
-		return (ans >= 0) ? ans : ans + MOD;
-	}
+    int n;
+    vector<ll> h[2], pot[2];
+
+    hashing(string s) {
+        n = s.size();
+        for (int i = 0; i < 2; ++i) {
+            h[i].resize(n + 1);
+            pot[i].resize(n + 1, 1);
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 2; ++j) {
+                h[j][i] = (h[j][i - 1] * p[j] + s[i - 1]) % MOD[j];
+                pot[j][i] = (pot[j][i - 1] * p[j]) % MOD[j];
+            }
+        }
+    }
+
+    ll hashValue(int i, int j) {
+        ll a = mod(h[0][j] - mod(h[0][i] * pot[0][j - i], MOD[0]), MOD[0]);
+        ll b = mod(h[1][j] - mod(h[1][i] * pot[1][j - i], MOD[1]), MOD[1]);
+        return a*X + b;
+    }
 };
