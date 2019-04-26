8a7a53eda21e375a4a68b0b568351be33536e200
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-04-26 13:10:40 -0500

    Update String Hashing.cpp

--- c++/String/String Hashing.cpp
+++ c++/String/String Hashing.cpp
@@ -1,32 +1,30 @@
 Estructura para realizar operaciones de hashing. 
 
-#define mod(a, b) (((a)%(b)+(b))%(b))
-
-ll p[] = {153, 255};
-ll MOD[] = {1000000007, 1000000009};
-ll X = 1e9 + 10;
-
+long long p[] = {257, 359};
+long long mod[] = {1000000007, 1000000009};
+long long X = 1e9+10;
+ 
 struct hashing {
-    int n;
-    vector<ll> h[2], pot[2];
-
+    vector<long long> h[2], pot[2];
+	int n;
+ 
     hashing(string s) {
         n = s.size();
         for (int i = 0; i < 2; ++i) {
             h[i].resize(n + 1);
             pot[i].resize(n + 1, 1);
         }
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; ++i) {
             for (int j = 0; j < 2; ++j) {
-                h[j][i] = (h[j][i - 1] * p[j] + s[i - 1]) % MOD[j];
-                pot[j][i] = (pot[j][i - 1] * p[j]) % MOD[j];
+                h[j][i] = (h[j][i - 1] * p[j] + s[i - 1]) % mod[j];
+                pot[j][i] = (pot[j][i - 1] * p[j]) % mod[j];
             }
         }
     }
-
-    ll hashValue(int i, int j) {
-        ll a = mod(h[0][j] - mod(h[0][i] * pot[0][j - i], MOD[0]), MOD[0]);
-        ll b = mod(h[1][j] - mod(h[1][i] * pot[1][j - i], MOD[1]), MOD[1]);
+	//Hash del substring en el rango [i, j)
+    long long hashValue(int i, int j) {
+        long long a = (h[0][j] - (h[0][i] * pot[0][j - i] % mod[0])) % mod[0];
+        long long b = (h[1][j] - (h[1][i] * pot[1][j - i] % mod[1])) % mod[1];
         return a*X + b;
     }
 };
