58da4bbf79dbc11661a0ad0e98bf292d7523c7f9
Jhoser Pacheco <44657472+jhoserpacheco@users.noreply.github.com>
2019-04-26 15:14:03 -0500

    Update Sieve of Eratosthenes

--- java/Math/Sieve of Eratosthenes.java
+++ java/Math/Sieve of Eratosthenes.java
@@ -3,17 +3,22 @@ Para saber si p es un número primo, hacer: if (!marked[p])
 
 static int MAX = 1000000;
 static int SQRT = 1000;
-static ArrayList<Integer> primes = new ArrayList<Integer>();
+static ArrayList<Integer> primes = new ArrayList<>();
 static boolean marked[] = new boolean[MAX+1];
 
 static void sieve() {
-	marked[1] = true;
-	primes.add(2);
-	for (int i = 4; i <= MAX; i += 2) marked[i] = true;
-	int i = 3;
-	for (; i <= SQRT; i += 2) if (!marked[i]) {
-		primes.add(i);
-		for (int j = i*i; j <= MAX; j += i*2) marked[j] = true;
-	}
-	for (; i <= MAX; i += 2) if (!marked[i]) primes.add(i);
-}
+    marked[1] = true;
+    int i = 2;
+    for (; i <= SQRT; ++i) 
+	if (!marked[i]) {
+	    primes.add(i);
+	    for (int j = i*i; j <= MAX; j += i) {
+		marked[j] = true;
+	    }
+	} 
+    for (; i <= MAX; i += 2){
+        if (!marked[i]) {
+	    primes.add(i);
+	}  
+    }
+} 
