2135f5babc2783cd6d8e596e766091edcaa32ec2
Crisel Ayala <criseljazminal@ufps.edu.co>
2019-04-29 17:02:46 -0500

    Update KMP's Algorithm.java

--- java/String/KMP's Algorithm.java
+++ java/String/KMP's Algorithm.java
@@ -1,12 +1,15 @@
 Encuentra si el string pattern se encuentra en el string cadena. Debe estar definido el método prefix_function.
 
+import java.util.ArrayList;
+
 static boolean kmp(String cadena, String pattern) {
 	int n=cadena.length();
 	int m=pattern.length();
-	int[] tab=prefix_function(pattern);
+	ArrayList<Integer> tab=prefix_function(pattern);
+
 	for(int i = 0, seen = 0; i < n; i++) {
 		while(seen > 0 && cadena.charAt(i) != pattern.charAt(seen)) {
-			seen = tab[seen-1];
+			seen = tab.get(seen-1);
 		}
 		if(cadena.charAt(i) == pattern.charAt(seen)) seen++;
 		if(seen == m) return true;
