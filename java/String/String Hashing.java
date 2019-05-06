93602670a4d0f505a183c9b6c1444901cad875b9
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-05-05 19:14:18 -0500

    Update String Hashing.java

--- java/String/String Hashing.java
+++ java/String/String Hashing.java
@@ -19,15 +19,15 @@ static class Hashing {
 		}
 		for (int i = 1; i <= n; ++i) {
 			for (int j = 0; j < 2; ++j) {
-				h[j][i] = (h[j][i - 1] * p[j] + s[i - 1]) % mod[j];
-				pot[j][i] = (pot[j][i - 1] * p[j]) % mod[j];
+				h[j][i] = (h[j][i-1] * p[j] + s[i-1]) % mod[j];
+				pot[j][i] = (pot[j][i-1] * p[j]) % mod[j];
 			}
 		}
 	}
 	//Hash del substring en el rango [i, j)
 	long hash(int i, int j) {
-		long a = (h[0][j] - (h[0][i] * pot[0][j - i] % mod[0])) % mod[0];
-		long b = (h[1][j] - (h[1][i] * pot[1][j - i] % mod[1])) % mod[1];
+		long a = (h[0][j] - (h[0][i] * pot[0][j-i] % mod[0]) + mod[0]) % mod[0];
+		long b = (h[1][j] - (h[1][i] * pot[1][j-i] % mod[1]) + mod[1]) % mod[1];
 		return a*X + b;
 	}
 }
