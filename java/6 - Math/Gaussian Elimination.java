a6590672f54965303a18e3c7a0c20f0afdd01965
Elizabeth Ramirez <eramirezvillamizar@gmail.com>
2019-05-27 15:32:15 -0500

    Update Gaussian Elimination.java

--- java/6 - Math/Gaussian Elimination.java
+++ java/6 - Math/Gaussian Elimination.java
@@ -1,46 +1,53 @@
-Resuelve sistemas de ecuaciones lineales por eliminación Gaussiana. matrix contiene los valores de la matriz cuadrada y result los resultados de las ecuaciones. Retorna un vector con el valor de las n incongnitas. Los resultados pueden necesitar redondeo.
+Resuelve sistemas de ecuaciones lineales por eliminación Gaussiana. mat[][] contiene los valores de la matriz cuadrada y los resultados de las ecuaciones en la ultima columna. Retorna un vector<> con el valor de las n incongnitas. Los resultados pueden necesitar redondeo.
 
-import java.util.ArrayList;
+const int MAX_N = 100;
+double mat[MAX_N][MAX_N + 1];
+int n;
 
-static int MAX = 100;
-static int n = 3;
-static double matrix[][] = new double[MAX][MAX];
-static double result[] = new double[MAX];
+vector<double> gauss() {
+    vector<double> vec(n-1);
+    for (int i = 0; i < n - 1; i++) {
+        int pivot = i;
+        for (int j = i + 1; j < n; j++)
+            if (abs(mat[j][i]) > abs(mat[pivot][i])) pivot = j;
+        for (int j = i; j <= n; j++)
+            swap(mat[i][j], mat[pivot][j]);
+        for (int j = i + 1; j < n; j++)
+            for (int k = n; k >= i; k--)
+                mat[j][k] -= mat[i][k]*mat[j][i] / mat[i][i];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        double temp = 0.0;
+        for (int j = i + 1; j < n; j++) temp += mat[i][j]*vec[j];
+        vec[i] = (mat[i][n]-temp) / mat[i][i];
+    }
+    return vec;
+}
 
-static ArrayList<Double> gauss() {
-	ArrayList<Double> ans = new ArrayList<Double>();
-  for(int i = 0; i < n; i++) ans.add(0.0);
-  double temp;
-	for (int i = 0; i < n; i++) {
-    int pivot = i;
-	  for (int j = i + 1; j < n; j++) {
-	  	temp = Math.abs(matrix[j][i]) - Math.abs(matrix[pivot][i]);
-	    if (temp > 0.000001) pivot = j;
-	  }
-	  double temp2[] = new double[n];
-	  System.arraycopy(matrix[i],0,temp2,0,n);
-	  System.arraycopy(matrix[pivot],0,matrix[i],0,n);
-	  System.arraycopy(temp2,0,matrix[pivot],0,n);
-	  temp = result[i];
-	  result[i] = result[pivot];
-	  result[pivot] = temp;  
-	  if (!(Math.abs(matrix[i][i]) < 0.000001)) {	
-	    for (int k = i + 1; k < n; k++) {
-		   	temp = -matrix[k][i] /  matrix[i][i];
-		   	matrix[k][i] = 0;
-		   	for (int l = i + 1; l < n; l++) {
-		     	matrix[k][l] += matrix[i][l] * temp;
-		    }
-	  		result[k] += result[i] * temp;
-	  	}
-	  }
-  }
-  for (int m = n - 1; m >= 0; m--) {
-   	temp = result[m];
-   	for (int i = n - 1; i > m; i--) {
-   		temp -= ans.get(i) * matrix[m][i];
-   	}
-   	ans.set(m,temp / matrix[m][m]);
-  }
-  return ans;
+
+static int MAX_N = 100;
+static double mat[][] = new double[MAX_N][MAX_N + 1];
+static int n;
+
+ArrayList<Double> gauss() {
+    ArrayList<Double> vec = new ArrayList(n-1);
+    for (int i = 0; i < n - 1; i++) {
+        int pivot = i;
+        for (int j = i + 1; j < n; j++)
+            if (Math.abs(mat[j][i]) > Math.abs(mat[pivot][i])) pivot = j;
+        for (int j = i; j <= n; j++){
+            double aux = mat[pivot][j];
+            mat[pivot][j] = mat[i][j];
+            mat[i][j] = aux;
+        }
+        for (int j = i + 1; j < n; j++)
+            for (int k = n; k >= i; k--)
+                mat[j][k] -= mat[i][k]*mat[j][i] / mat[i][i];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        double temp = 0.0;
+        for (int j = i + 1; j < n; j++) temp += mat[i][j]*vec.get(j);
+        vec.set(i, (mat[i][n]-temp) / mat[i][i]);
+    }
+    return vec;
 }
