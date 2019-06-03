75ae0ed81b97eb712ce19f552a284c9ef583bd8a
Elizabeth Ramirez <eramirezvillamizar@gmail.com>
2019-06-03 18:00:02 -0500

    Update LCA

--- java/5 - Graphs/Lowest Common Ancestor.java
+++ java/5 - Graphs/Lowest Common Ancestor.java
@@ -1,63 +1,82 @@
-Dados los nodos a y b de un arbol determina cual es el ancestro comun mas bajo entre a y b.
-*Tambien puede determinar la arista de peso maximo entre los nodos a y b (Para esto quitar los "//" y agregar la clase Node)
+Dados los nodos u y v de un arbol determina cual es el ancestro comun mas bajo entre u y v.
+*Tambien puede determinar la arista de peso maximo entre los nodos u y v (Para esto quitar los "//")
 SE DEBE EJECUTAR EL METODO build() ANTES DE UTILIZARSE
 
-static int n, e; //Cantidad de nodos y aristas 
-static int MAX = 100005; //Cantidad maxima de nodos
-static int LOG2 = 17; //log2(MAX)
-//static ArrayList<Node> ady[] = new ArrayList[MAX]; //Lista de adyacencia
-static ArrayList<Integer> ady[] = new ArrayList[MAX]; //Lista de adyacencia
+static final int MAX = 100005; //Cantidad maxima de nodos
+static final int LOG2 = 17; //log2(MAX)+1
+//ArrayList<edge> g[] = new ArrayList[MAX]; //Lista de adyacencia
+static ArrayList<Integer> g[] = new ArrayList[MAX]; //Lista de adyacencia
 static int dep[] = new int[MAX]; //Almacena la profundidad de cada nodo
 static int par[][] = new int[MAX][LOG2]; //Almacena los padres para responder las consultas
-//static int rmq[][] = new int[MAX][LOG2]; //Almacena los pesos para responder las consultas
+//int rmq[][] = new int[MAX][LOG2]; //Almacena los pesos para responder las consultas
+static int N, M; //Cantidad de nodos y aristas 
 
-static int lca(int a, int b) {
-	//int ans = -1;
-    if (dep[a] < dep[b]) { int c=a;a=b;b=c; }
-    int diff = dep[a] - dep[b];
+/*static class edge {
+    int v, w; 
+    
+    edge(int _v, int _w){
+        v = _v;
+        w = _w;
+    }
+};*/
+
+static int lca(int u, int v) {
+    //int ans = -1;
+    if (dep[u] < dep[v]){
+        int aux = u;
+        u = v;
+        v = aux;
+    }
+    int diff = dep[u] - dep[v];
     for (int i = LOG2-1; i >= 0; i--) {
-        if ((diff & (1 << i)) > 0) {
-        	//ans = Math.max(ans, rmq[a][i]);
-        	a = par[a][i];
+        if ((diff & (1 << i))> 0) {
+            //ans = Math.max(ans, rmq[u][i]);
+            u = par[u][i];
         }
     }
-    //if (a == b) return ans;
-	if (a == b) return a;
+    //if (u == v) return ans;
+    if (u == v) return u;
     for (int i = LOG2-1; i >= 0; i--) {
-        if (par[a][i] != par[b][i]) {
-        	//ans = Math.max(ans, Math.max(rmq[a][i], rmq[b][i]));
-            a = par[a][i];
-            b = par[b][i];
+        if (par[u][i] != par[v][i]) {
+            //ans = Math.max(ans, Math.max(rmq[u][i], rmq[v][i]));
+            u = par[u][i];
+            v = par[v][i];
         }
     }
-	//return Math.max(ans, Math.max(rmq[a][0], rmq[b][0]));
-	return par[a][0];
+    //return Math.max(ans, Math.max(rmq[u][0], rmq[v][0]));
+    return par[u][0];
 }
 
 static void dfs(int u, int p, int d) {
-	dep[u] = d;
-	par[u][0] = p;
-	for (int v /*Node nx*/ : ady[u]) {
-		//int v = nx.adjacent;
-		if (v != p) {
-			//rmq[v][0] = nx.cost;
-			dfs(v, u, d + 1);
-		}
-	}
+    dep[u] = d;
+    par[u][0] = p;
+    for (int v /* edge ed*/ : g[u]) {
+        //int v = ed.v;
+        if (v != p) {
+            //rmq[v][0] = ed.w;
+            dfs(v, u, d + 1);
+        }
+    }
 }
 
 static void build() {
-	for(int i = 0; i < n; i++) dep[i] = -1;
-	for(int i = 0; i < n; i++) {
-		if(dep[i] == -1) {
-			//rmq[i][0] = -1;
-			dfs(i, i, 0);
-		}
-	}
-	for(int j = 0; j < LOG2-1; j++) {
-		for(int i = 0; i < n; i++) {
-			par[i][j+1] = par[ par[i][j] ][j];
-			//rmq[i][j+1] = Math.max(rmq[ par[i][j] ][j], rmq[i][j]);
-		}
+    for(int i = 0; i < N; i++) dep[i] = -1;
+    for(int i = 0; i < N; i++) {
+        if(dep[i] == -1) {
+            //rmq[i][0] = -1;
+            dfs(i, i, 0);
+        }
+    }
+    for(int j = 0; j < LOG2-1; j++) {
+        for(int i = 0; i < N; i++) {
+            par[i][j+1] = par[ par[i][j] ][j];
+            //rmq[i][j+1] = Math.max(rmq[ par[i][j] ][j], rmq[i][j]);
+        }
+    }
+}
+
+static void init() {
+	for (int i = 0; i <= N; i++) {
+		g[i] = new ArrayList<>();
 	}
 }
