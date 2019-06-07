0dd5ebc48059cb7c9e9d3c0e7e6c8f32214e58b6
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-06-07 17:06:32 -0500

    Añadir descripción en Manacher.cpp

--- c++/7 - String/Manacher.cpp
+++ c++/7 - String/Manacher.cpp
@@ -1,3 +1,6 @@
+Devuelve un vector P donde para cada i P[i] es igual al largo del palindromo más largo con centro en i.
+Tener en cuenta que el string debe tener el siguiente formato: %#s[0]#s[1]#...#s[n-1]#$ (s es el string original y n es el largo del string)
+
 vector<int> manacher(string S) {
 	int n = S.size();
 	vector<int> P(n, 0);
