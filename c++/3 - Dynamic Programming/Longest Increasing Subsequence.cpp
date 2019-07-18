d24991da807055240125e9ad3f82e8ba7f8498af
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-07-18 18:31:30 -0500

    Update Longest Increasing Subsequence.cpp

--- c++/3 - Dynamic Programming/Longest Increasing Subsequence.cpp
+++ c++/3 - Dynamic Programming/Longest Increasing Subsequence.cpp
@@ -1,27 +1,72 @@
-Halla la longitud de la subsecuencia creciente mas larga. MAX debe definirse en el tamaño  limite del array, n es el tamaño del array. Si se admiten valores repetidos, cambiar el < de I[mid] <= values[i] por <=
+//METODO PARA CALCULAR EL LIS en O(n^2) y O(nlog(n)). La ventaja de tener a mano O(n^2) es porque es mas facil de codear, entender y modificar
 
-const int inf = 2000000000;
-const int MAX = 100000;
-int n; 
-int values[MAX + 5];
-int L[MAX + 5]; 
-int I[MAX + 5]; 
+const int MAX = 1000;
 
-int lis() { 
- 	int i, low, high, mid;
- 	I[0] = -inf; 
- 	for (i = 1; i <= n; i++) I[i] = inf;
- 	int ans = 0;
- 	for(i = 0; i < n; i++) {
- 		low = mid = 0;
- 		high = ans;
- 		while(low <= high) {
- 			mid = (low + high) / 2;
- 			if(I[mid] < values[i]) low = mid + 1;
- 			else high = mid - 1;
- 		} 
- 		I[low] = values[i];
- 		if(ans < low) ans = low;
+int A[MAX];
+int dp[MAX];
+int N = MAX;
+vector<int> LIS; //PARA Lis_opt
+	
+
+//LIS O(n^2).Si es non-decreasing cambiar (i > j) por (i >= j)
+int lis(){
+	int best = -1;
+	for(int i = 0; i < N; i++){
+		dp[i] = 1;
+		for(int j = 0; j < N; j++){
+			if(A[i] > A[j]) dp[i] = max(dp[i], dp[j] + 1);			
+		}
+		best = max(best, dp[i]);
 	}
- 	return ans;
+	return best;
+}
+
+
+//LIS O(nlog(n)) Para longest non-decreasing cambiar lower_bound por upper_bound
+int lis_opt(){
+	LIS.clear();
+	for(int i = 0; i < N; i++){
+		auto id = lower_bound(LIS.begin(), LIS.end(), A[i]);
+		if(id == LIS.end()) LIS.push_back(A[i]);
+		else{
+			int idx = id - LIS.begin();
+			LIS[idx] = A[i];
+		}
+	}
+	return LIS.size();
+}
+
+//Metodo para calcular y además reconstruir el LIS O(nlogn)
+int build_lis(){
+	LIS.clear();
+	int parent[N], last;
+	vector<int> rb;
+	for(int i = 0; i < N; i++){
+		auto id = lower_bound(LIS.begin(), LIS.end(), A[i]);
+		if(id == LIS.end()){ 
+			LIS.push_back(A[i]);
+			rb.push_back(i);
+			if(i) parent[i] = rb[rb.size() - 2];
+			else parent[i] = -1;
+			last = i;
+		}
+		else{
+			int idx = id - LIS.begin();
+			LIS[idx] = A[i];
+			rb[idx] = i;
+			parent[i] = rb[idx - 1]; 
+		}
+	}
+	//Reconstruye el LIS
+	vector<int> aux;
+	while(last!=-1){
+		aux.push_back(A[last]);
+		last = parent[last];
+	}
+	
+	for(int i = aux.size() - 1; i >= 0; i--){
+		cout << aux[i] <<" ";
+	}
+	cout << '\n';
+	return LIS.size();	
 }
