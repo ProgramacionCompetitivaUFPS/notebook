b0a94e7cb0006c14f779d13b747984fe62af7ce2
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-07-18 15:58:20 -0500

    Update Knapsack.cpp

--- c++/3 - Dynamic Programming/Knapsack.cpp
+++ c++/3 - Dynamic Programming/Knapsack.cpp
@@ -1,23 +1,52 @@
-Dados N articulos, cada uno con su propio valor y peso y un tamaño maximo de una mochila, se debe calcular el valor maximo de los elementos que es posible llevar.
-Debe seleccionarse un subconjunto de objetos, de tal manera que quepan en la mochila y representen el mayor valor posible.
+const int MAX_N = 1000;
+const int MAX_W = 10000;
+const int INF = (int) (2e9);
 
-#include <algorithm>
+int dp[MAX_N + 5][MAX_W + 5];
+int gold[MAX_N];
+int weight[MAX_N];
+int N;
+vector<int> rb;
 
-const int MAX_WEIGHT = 40;//Peso maximo de la mochila
-const int MAX_N = 1000; //Numero maximo de objetos
-int N;//Numero de objetos 
-int prices[MAX_N];//precios de cada producto
-int weights[MAX_N];//pesos de cada producto
-int memo[MAX_N][MAX_WEIGHT];//tabla dp
-
-//El metodo debe llamarse con 0 en el id, y la capacidad de la mochila en w
-int knapsack(int id, int w) {
-  if (id == N || w == 0) return 0;
-  if (memo[id][w] != -1) return memo[id][w];
-  if (weights[id] > w) memo[id][w] = knapsack(id + 1, w);
-  else memo[id][w] = max(knapsack(id + 1, w), prices[id] + knapsack(id + 1, w - weights[id]));
-  return memo[id][w];
+//mochila TOP_DOWN. NECESITA INICIALIZARSE ANTES DP EN -1
+int f(int i, int w){
+	if(w < 0) return -INF;
+	if(i == N) return 0;
+	int &ans = dp[i][w];
+	if(ans != -1) return ans;
+	ans = max(f(i + 1, w), f(i + 1, w - weight[i]) + gold[i]);
+	return ans;
 }
 
-//La tabla memo debe iniciar en -1
-memset(memo, -1, sizeof(memo[0][0]) * MAX_N * MAX_WEIGHT);
+//BOTTOM_UP MOCHILA. ACCEDER COMO dp[0][W]
+void mochila(){
+	for(int i = 0; i <= MAX_W; i++) dp[N][i] = 0; 
+	for(int i = N - 1; i >= 0; i--){
+		for(int w = 0; w <= MAX_W; w++){
+			dp[i][w] = dp[i + 1][w];
+			if(w - weight[i] >= 0) dp[i][w] = max(dp[i][w], dp[i + 1][w - weight[i]] + gold[i]);
+		}
+	}
+}
+
+//MOCHILA OPTIMIZANDO MEMORIA. ACCEDER COMO dp_opt[0][W]
+int dp_opt[2][MAX_W + 5];
+void mochila_opt(){
+	for(int i = 0; i <= MAX_W; i++) dp[N%2][i] = 0; 
+	for(int i = N - 1; i >= 0; i--){
+		for(int w = 0; w <= MAX_W; w++){
+			dp_opt[i%2][w] = dp_opt[(i + 1)%2][w];
+			if(w - weight[i] >= 0) dp_opt[i%2][w] = max(dp_opt[i%2][w], dp_opt[(i + 1)%2][w - weight[i]] + gold[i]);
+		}
+	}
+}
+
+//RECONSTRUIR SOLUCION. GUARDA LOS INDICES DE LOS ELEMENTOS USADOS. NO FUNCIONA CON MOCHILA OPTIMIZADA. 
+//ADVERTENCIA: Si existen multiples soluciones reconstruye la que primero aparezca. Para la ultima recorrer al revés
+void build(int W){
+	rb.clear();
+	for(int i = 0; i < N && W > 0; i++){
+		if(W - weight[i] >= 0 && dp[i][W] == dp[i + 1][W - weight[i]] + gold[i])
+			rb.push_back(i);
+	}
+}
