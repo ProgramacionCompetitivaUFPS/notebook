d422267ca3be079177fdc1d76dfaf17adcab476f
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-07-18 12:19:43 -0500

    Create Ternary Search.cpp

---
+++ c++/8 - Utilities/Ternary Search.cpp
@@ -0,0 +1,17 @@
+Retorna el valor minimo de una funcion entre l y r. Se recomienda usar 50 iteraciones.
+
+double f(double x){
+	return //funcion a evaluar que depende de x
+}
+
+double ternary_search(double l, double r, int it){
+    double a = (2.0*l + r)/3.0;
+    double b = (l + 2.0*r)/3.0;
+    
+    if(it == 0){
+      return f(a);  
+    }
+    
+    if(f(a) < f(b)) return ternary_search(l, b, it-1);
+    return ternary_search(a, r, it-1);
+}
