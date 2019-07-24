478f1308a9c6fae67676bfb60179d92ab0b91680
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-07-24 13:23:20 -0500

    Add build Block cut tree

--- c++/5 - Graph/Articulation Bridges Biconnected.cpp
+++ c++/5 - Graph/Articulation Bridges Biconnected.cpp
@@ -1,4 +1,4 @@
-Dado un grafo no dirigido halla los puntos de articulación, puentes y componentes biconexas.
+Dado un grafo no dirigido halla los puntos de articulación, puentes y componentes biconexas. Para construir el block cut tree quitar los comentarios.
 
 struct edge {
 	int u, v, comp; //A que componente biconexa pertenece
@@ -10,49 +10,74 @@ vector<int> g[MAX]; //Lista de adyacencia
 vector<edge> e; //Lista de aristas
 stack<int> st;
 int low[MAX], num[MAX], cont;
-int art[MAX]; //si != 0 el nodo es un punto de articulacion 
-int cantBC; //Cantidad de componentes biconexas
+bitset<MAX> art; //Si el nodo es un punto de articulacion
+//vector<set<int>> comps; //Componentes biconexas
+//<vector<int>> tree; //Block cut tree
+//vector<int> id; //Id del nodo en el block cut tree
+int cantBCC; //Cantidad de componentes biconexas
 int N, M; //Cantidad de nodos y aristas
 
 void add_edge(int u, int v){
-	g[u].pb(e.size());
-	g[v].pb(e.size());
-	e.pb({u, v, -1, false});
+	g[u].push_back(e.size());
+	g[v].push_back(e.size());
+	e.push_back({u, v, -1, false});
 }
 
 void dfs(int u, int p = -1) {
 	low[u] = num[u] = cont++;
-	for (int id : g[u]) {
-		edge &ed = e[id];
+	for (int i : g[u]) {
+		edge &ed = e[i];
 		int v = ed.u^ed.v^u;
 		if (num[v] == -1) {
-			st.push(id);
-			dfs(v, id);
+			st.push(i);
+			dfs(v, i);
 			if (low[v] > num[u])
 				ed.bridge = true; //bridge
 			if (low[v] >= num[u]) {
-				art[u]++; //articulation
+				art[u] = (num[u] > 0 || num[v] > 1); //articulation
 				int last; //start biconnected
+				//comps.push_back({});
 				do {
 					last = st.top(); st.pop();
-					e[last].comp = cantBC;
-				} while (last != id);
-				cantBC++; //end biconnected
+					e[last].comp = cantBCC;
+					//comps.back().insert(e[last].u);
+					//comps.back().insert(e[last].v);
+				} while (last != i);
+				cantBCC++; //end biconnected
 			}
 			low[u] = min(low[u], low[v]);
-		} else if (id != p && num[v] < num[u]) {
-			st.push(id);
+		} else if (i != p && num[v] < num[u]) {
+			st.push(i);
 			low[u] = min(low[u], num[v]);
 		}
 	}
-	art[u] -= (p == -1);
+}
+
+void build_tree() {
+	tree.clear(); id.resize(N);
+	for (int u = 0; u < N; u++) {
+		if (art[u]) {
+			id[u] = tree.size();
+			tree.push_back({});
+		}
+	}
+	for (auto &comp : comps) {
+		int node = tree.size();
+		tree.push_back({});
+		for (int u : comp) {
+			if (art[u]) {
+				tree[id[u]].push_back(node);
+				tree[node].push_back(id[u]);
+			} else id[u] = node;
+		}
+	}
 }
 
 void init() {
-    cont = cantBC = 0;
-    for (int i = 0; i <= N; i++) {
-        g[i].clear();
-        num[i] = -1; //no visit
-        art[i] = 0; //no articulation point
-    }
+	art = cont = cantBCC = 0;
+	//comps.clear();
+	for (int i = 0; i <= N; i++) {
+		g[i].clear();
+		num[i] = -1; //no visit
+	}
 }
