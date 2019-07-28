43825a45658e8184cbde2bc7b706f7743a185b2a
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-07-28 12:46:57 -0500

    Update Sparse Table.cpp

--- c++/2 - Data Structures/Sparse Table.cpp
+++ c++/2 - Data Structures/Sparse Table.cpp
@@ -1,25 +1,25 @@
 Estructura de datos que permite procesar consultas por rangos.
 
-const int MAX_N = 1000;
-const int K = log2(MAX_N)+1;
-int st[MAX_N][K];
-int _log2[MAX_N+1];
-int A[MAX_N];
-int n;
+const int MAX = 1000;
+const int LOG = log2(MAX_N)+1;
+int spt[LOG][MAX];
+int _log2[MAX+1];
+int arr[MAX];
+int N;
 
 void calc_log2() {
     _log2[1] = 0;
-    for (int i = 2; i <= MAX_N; i++) _log2[i] = _log2[i/2] + 1;
+    for (int i = 2; i <= MAX; i++) _log2[i] = _log2[i/2]+1;
 }
 
 void build() {
-    for (int i = 0; i < n; i++) st[i][0] = A[i];
-    for (int j = 1; j <= K; j++)
-        for (int i = 0; i + (1 << j) <= n; i++)
-            st[i][j] = min(st[i][j-1], st[i + (1 << (j - 1))][j - 1]);
+    for (int i = 0; i < N; i++) spt[0][i] = arr[i];
+    for (int j = 0; j < LOG-1; j++) {
+        for (int i = 0; i+(1<<(j+1)) <= N; i++) {
+            spt[j+1][i] = min(spt[j][i], spt[j][i+(1<<j)]);
 }
 
 int rmq(int i, int j) {
     int k = _log2[j-i+1];
-    return min(st[i][k], st[j - (1 << k) + 1][k]);
+    return min(spt[k][i], spt[k][j-(1<<k)+1]);
 }
