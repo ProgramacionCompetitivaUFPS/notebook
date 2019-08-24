cc63d91485ad3cbe098a55acbfffeafa83de4591
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-08-24 10:45:14 -0500

    Update Utilities

--- c++/8 - Utilities/Split String.cpp
+++ c++/8 - Utilities/Split String.cpp
@@ -1,7 +1,7 @@
 Divide el string s por espacios. Devuelve un vector<> con los substrings resultantes.
 * Tambien puede dividir el string s por cada ocurrencia de un char c. (Para esto quitar los "//")
 
-vector<string> split(string s/*, char c*/) {
+vector<string> split(string &s/*, char c*/) {
     vector<string> v;
     istringstream iss(s);
     string sub;
