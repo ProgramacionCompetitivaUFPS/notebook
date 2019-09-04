a46c3311cd99cc46252b1a20e06107a1a3f19d1e
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-09-03 22:40:25 -0500

    Update Utilities

--- java/8 - Utilities/Binary search.java
+++ java/8 - Utilities/Binary search.java
@@ -1,7 +1,7 @@
 Dado un arreglo ordenado ascendentemente de tamaño n, busca el elemento x y devuelve su posición, si no lo encuentra devuelve -1.
 
-static int binary_search(int array[], int n, int x){
-	int l = 0; r = n-1;
+static int binary_search(int array[], int x){
+	int l = 0, r = arr.length-1;
 	while (l <= r) {
 		int m = (l+r)/2;
 		if(array[m] < x) l = m+1;
