dcb2cc2faba6e2dcc9af6b362d0105ced22c9e08
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-09-18 21:47:09 -0500

    Update Floyd Warshall.cpp

--- c++/5 - Graph/Floyd Warshall.cpp
+++ c++/5 - Graph/Floyd Warshall.cpp
@@ -1,6 +1,6 @@
 Dado un grafo halla la distancia mínima entre cualquier par de nodos. g[i][j] guardará la distancia mínima entre el nodo i y el j.
 
-#define INF (1<<30)
+const int INF = 1e9;
 
 const int MAX = 505; //Cantidad maxima de nodos
 int g[MAX][MAX]; //Matriz de adyacencia
@@ -16,7 +16,7 @@ void floydWarshall() {
 void init() {
     for(int i = 0; i <= N; i++) {
         for(int j = 0; j <= N; j++) {
-            g[i][j] = INF;
+            g[i][j] = (i==j ? 0 : INF);
         }
     }
 }
