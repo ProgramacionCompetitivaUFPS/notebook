cfd2e622047016a1d2e8b0c5bf8a367a407ef05a
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-10-08 15:17:13 -0500

    Create Chinese Remainder Theorem.cpp

---
+++ c++/6 - Math/Chinese Remainder Theorem.cpp
@@ -0,0 +1,16 @@
+Encuentra un x tal que para cada i : x es congruente con A_i mod M_i
+Devuelve {x, lcm}, donde x es la solución con modulo lcm (lcm = LCM(M_0, M_1, ...)). Dado un k : x + k*lcm es solución tambien.
+Si la solución no existe o la entrada no es válida devuelve {-1, -1}
+
+pair<int, int> crt(vector<int> A, vector<int> M) {
+    int n = A.size(), ans = A[0], lcm = M[0];
+    for (int i = 1; i < n; i++) {
+        int d = euclid(lcm, M[i]);
+        if ((A[i] - ans) % d) return {-1, -1};
+        int mod = lcm / d * M[i];
+        ans = (ans + x * (A[i] - ans) / d % (M[i] / d) * lcm) % mod;
+        if (ans < 0) ans += mod;
+        lcm = mod;
+    }
+    return {ans, lcm};
+}
