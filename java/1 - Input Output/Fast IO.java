8da1fd56a3293abc83eb58f1e2303af555fa0a26
Brayan Arias <stevenson.arias@gmail.com>
2019-10-20 13:07:01 -0500

    Modify IO
    static is wrong

--- java/1 - Input Output/Fast IO.java
+++ java/1 - Input Output/Fast IO.java
@@ -1,5 +1,5 @@
-static Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 
 //Obligatorio al finalizar la impresion
 so.close();
