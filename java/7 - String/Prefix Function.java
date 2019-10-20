4a25acdb9a2d4bdc6164bd08c4a9698a0ae5ff89
Brayan Arias <stevenson.arias@gmail.com>
2019-10-20 16:31:00 -0500

    Create Prefix Function.java

---
+++ java/7 - String/Prefix Function.java
@@ -0,0 +1,16 @@
+Dado un string s retorna un array pf donde pf[i] es el largo del prefijo propio más largo
+que tambien es sufijo de cad[0] hasta cad[i].
+El String debe pasarse como un array de chars cadena.toCharArray();
+/// O(n)
+
+static int[] prefix_function(char[] cad) {
+    int n = cad.length;
+    int pf[] = new int[n];
+    pf[0] = 0;
+    for (int i = 1, j = 0; i < n; i++) {
+        while (j==1 && cad[i] != cad[j]) j = pf[j-1];
+        if (cad[i] == cad[j]) j++;
+        pf[i] = j;
+    }
+    return pf;
+}
