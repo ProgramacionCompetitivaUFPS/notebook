895967fadf40d09691bd5a7ac3f2efbea9d06ad9
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-11-04 23:14:00 -0500

    Update and rename Angle.cpp to Angle ABC.cpp

--- c++/4 - Geometry/Angle ABC.cpp
+++ c++/4 - Geometry/Angle ABC.cpp
@@ -1,8 +1,5 @@
 Dados 3 puntos A, B, y C, determina el valor del angulo ABC (origen en B) en radianes. IMPORTANTE: Definir la estructura point y vec (Geometric Vector). Si se desea convertir a grados sexagesimales, revisar Sexagesimal degrees and radians.
 
-#include <vector>
-#include <cmath>
-
 double angle(point a, point b, point c) { 
   vec ba = toVector(b, a);
   vec bc = toVector(b, c);
