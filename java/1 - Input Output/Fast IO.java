a38f91c549a4c793f81e4ea320478ad8b71e0aa6
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-12-15 11:01:55 -0500

    Update Fast IO.java

--- java/1 - Input Output/Fast IO.java
+++ java/1 - Input Output/Fast IO.java
@@ -2,4 +2,4 @@ Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 
 //Obligatorio al finalizar la impresion
-so.close();
+out.close();
