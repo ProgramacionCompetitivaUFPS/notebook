faf2e096e7aa763c84d6ecb1e01599a0827e471d
José Manuel Salazar <manuelsalazar1030@gmail.com>
2019-12-20 19:38:28 -0500

    Update KMP.cpp

--- c++/7 - String/KMP.cpp
+++ c++/7 - String/KMP.cpp
@@ -2,9 +2,8 @@ Cuenta las apariciones del string p en el string s. Agregar Prefix Function.
 
 /// O(n+m)
 int kmp(string &s, string &p) {
-    int n = s.size(), m = p.size();
+    int n = s.size(), m = p.size(), cnt = 0;
     vector<int> pf = prefix_function(p);
-    int cnt = 0;
     for(int i = 0, j = 0; i < n; i++) {
         while(j && s[i] != p[j]) j = pf[j-1];
         if(s[i] == p[j]) j++;
