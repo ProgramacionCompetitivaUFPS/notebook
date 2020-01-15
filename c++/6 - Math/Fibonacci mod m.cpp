9ee592154cd2c409686b063a45939407d37d7463
José Manuel Salazar <manuelsalazar1030@gmail.com>
2020-01-15 13:33:38 -0500

    Update Fibonacci mod m.cpp

--- c++/6 - Math/Fibonacci mod m.cpp
+++ c++/6 - Math/Fibonacci mod m.cpp
@@ -1,14 +1,14 @@
 Calcula fibonacci(n) % m.
 
 /// O(log(n))
-ll fibmod(ll n, ll m) {
-    ll a = 0, b = 1, c;
+int fibmod(ll n, int m) {
+    int a = 0, b = 1, c;
     for (int i = 63-__builtin_clzll(n); i >= 0; i--) {
         c = a;
-        a = ((c%m) * (2*(b%m) - (c%m) + m)) % m;
-        b = ((c%m) * (c%m) + (b%m) * (b%m)) % m;
-        if (((n >> i) & 1) != 0) {
-            c = (a + b) % m;
+        a = (1ll*c*(2ll*b-c+m)) % m;
+        b = (1ll*c*c + 1ll*b*b) % m;
+        if ((n>>i) & 1) {
+            c = (a+b) % m;
             a = b; b = c;
         }
     }
