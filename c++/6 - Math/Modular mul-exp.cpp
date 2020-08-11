711996f3a3f46cbf877f7e2b6e3edeef8ef929fc
José Manuel Salazar <manuelsalazar1030gmail.com>
2020-08-10 21:52:47 -0500

    Update bit manipulation and modexp

---
+++ c++/6 - Math/Modular mul-exp.cpp
@@ -0,0 +1,20 @@
+En ambos métodos a y b deben envíarse ya con módulo m.
+
+* Calcular (a*b) % m sin overflow.
+/// O(1)
+ll mul(ll a, ll b, ll m) {
+	ll r = a*b-(ll)((long double)a*b/m+.5)*m;
+	return r < 0 ? r+m : r;
+}
+
+* Calcular (b^e) % m (e puede ser ll). Si m es ll usar mul() sin overflow.
+/// O(log(e))
+int pow(int b, int e, int m) {
+    int ans = 1;
+    while (e) {
+        if (e&1) ans = (1ll*ans*b) % m;
+        b = (1ll*b*b) % m;
+        e /= 2;
+    }
+    return ans;
+}
