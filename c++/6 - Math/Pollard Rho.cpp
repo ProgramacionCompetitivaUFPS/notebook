564cb02c668c6b947bc2f49bbe27da66af3206f2
JoseManuelSalazar <manuelsalazar1030@gmail.com>
2020-08-23 13:18:41 -0500

    New pollard Rho

--- c++/6 - Math/Pollard Rho.cpp
+++ c++/6 - Math/Pollard Rho.cpp
@@ -1,7 +1,7 @@
 La funcion Rho de Pollard calcula un divisor no trivial de n. Agregar Modular Multiplication.
 
 ll rho(ll n) {
-    if(!(n&1)) return 2;
+    if (!(n&1)) return 2;
     ll x = 2, y = 2, d = 1;
     ll c = rand() % n + 1;
     while (d == 1) {
