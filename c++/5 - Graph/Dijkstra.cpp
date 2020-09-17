82c7fdc4b25961eb88a470a8bfb16102caf034f1
JoseManuelSalazar <manuelsalazar1030@gmail.com>
2020-09-17 16:29:11 -0500

    Minor changes in dijkstra

--- c++/5 - Graph/Dijkstra.cpp
+++ c++/5 - Graph/Dijkstra.cpp
@@ -1,21 +1,19 @@
 Dado un grafo con pesos no negativos halla la ruta de costo mínimo entre un nodo inicial u y todos los demás nodos.
 
-#define INF (1ll<<62)
-
 struct edge {
-    int v;
-    long long w;
+    int v; ll w;
     
-    bool operator < (const edge &b) const {
-        return w > b.w; //Orden invertido
+    bool operator < (const edge &o) const {
+        return o.w < w; //invertidos para que la pq ordene de < a >
     }
 };
 
-const int MAX = 100005; //Cantidad maxima de nodos
-vector<edge> g[MAX]; //Lista de adyacencia
-bitset<MAX> vis; //Marca los nodos ya visitados
-int pre[MAX]; //Almacena el nodo anterior para construir las rutas
-long long dist[MAX]; //Almacena la distancia a cada nodo
+const ll inf = 1e18;
+const int MX = 1e5+5; //Cantidad maxima de nodos
+vector<edge> g[MX]; //Lista de adyacencia
+bool vis[MX]; //Marca los nodos ya visitados
+int pre[MX]; //Almacena el nodo anterior para construir las rutas
+ll dist[MX]; //Almacena la distancia a cada nodo
 int N, M; //Cantidad de nodos y aristas
 
 void dijkstra(int u) {
@@ -24,14 +22,13 @@ void dijkstra(int u) {
     dist[u] = 0;
     
     while (pq.size()) {
-        u = pq.top().v;
-        pq.pop();
+        u = pq.top().v; pq.pop();
         if (!vis[u]) {
             vis[u] = true;
-            for (auto nx : g[u]) {
-                int v = nx.v;
-                if(!vis[v] && dist[v] > dist[u] + nx.w) {
-                    dist[v] = dist[u] + nx.w;
+            for (auto &ed : g[u]) {
+                int v = ed.v;
+                if (!vis[v] && dist[v] > dist[u] + ed.w) {
+                    dist[v] = dist[u] + ed.w;
                     pre[v] = u;
                     pq.push({v, dist[v]});
                 }
@@ -43,7 +40,7 @@ void dijkstra(int u) {
 void init() {
     for(int i = 0; i <= N; i++) {
         g[i].clear();
-        dist[i] = INF;
+        dist[i] = inf;
         vis[i] = false;
     }
 }
