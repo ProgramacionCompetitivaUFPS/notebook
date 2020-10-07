4e28fdbb28bc4c90c10f20813c631ea8e00a2775
JoseManuelSalazar <manuelsalazar1030@gmail.com>
2020-10-07 14:04:30 -0500

    Add Maximum expression variant

--- c++/7 - String/Minimum Expression.cpp
+++ c++/7 - String/Minimum Expression.cpp
@@ -6,7 +6,7 @@ int minimum_expression(string s) {
     int len = s.size(), i = 0, j = 1, k = 0;
     while(i+k < len && j+k < len) {
         if(s[i+k] == s[j+k]) k++;
-        else if(s[i+k] > s[j+k]) i = i+k+1, k = 0;
+        else if(s[i+k] > s[j+k]) i = i+k+1, k = 0; // cambiar por < para maximum
         else j = j+k+1, k = 0;
         if(i == j) j++;
     }
