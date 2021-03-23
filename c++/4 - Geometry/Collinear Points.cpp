615fa7298f14849b960d04d017f008e27f336688
Elizabeth Villamizar <elizabeth@vtex.com.br>
2021-03-23 10:23:43 -0500

    Adding geometry files

--- c++/4 - Geometry/Collinear Points.cpp
+++
@@ -1,9 +0,0 @@
-Determina si el punto r está en la misma linea que los puntos p y q. IMPORTANTE: Deben incluirse las estructuras point y vec.
-
-
-double cross(vec a, vec b) { 
-	return a.x * b.y - a.y * b.x; 
-}
-bool collinear(point p, point q, point r) {
-	return fabs(cross(toVector(p, q), toVector(p, r))) < 1e-9; 
-}
