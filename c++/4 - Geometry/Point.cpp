77a36f597ec59362e41b0667a5a1f15444c5d3ce
Elizabeth Villamizar <elizabeth@vtex.com.br>
2021-03-28 12:44:16 -0500

    Fixing identation

--- c++/4 - Geometry/Point.cpp
+++ c++/4 - Geometry/Point.cpp
@@ -2,16 +2,16 @@ typedef double lf;
 const lf eps = 1e-9;
 typedef double T;
 struct pt {
-  T x, y;
-  pt operator + (pt p) { return {x+p.x, y+p.y}; }
-  pt operator - (pt p) { return {x-p.x, y-p.y}; }
-  pt operator * (pt p) { return {x*p.x-y*p.y, x*p.y+y*p.x}; }
-  pt operator * (T d) { return {x*d, y*d}; }
-  pt operator / (lf d) { return {x/d, y/d}; } /// only for floating point
-  bool operator == (pt b) { return x == b.x && y == b.y; }
-  bool operator != (pt b) { return !(*this == b); }
-  bool operator < (const pt &o) const { return y < o.y || (y == o.y && x < o.x); }
-  bool operator > (const pt &o) const { return y > o.y || (y == o.y && x > o.x); }
+    T x, y;
+    pt operator + (pt p) { return {x+p.x, y+p.y}; }
+    pt operator - (pt p) { return {x-p.x, y-p.y}; }
+    pt operator * (pt p) { return {x*p.x-y*p.y, x*p.y+y*p.x}; }
+    pt operator * (T d) { return {x*d, y*d}; }
+    pt operator / (lf d) { return {x/d, y/d}; } /// only for floating point
+    bool operator == (pt b) { return x == b.x && y == b.y; }
+    bool operator != (pt b) { return !(*this == b); }
+    bool operator < (const pt &o) const { return y < o.y || (y == o.y && x < o.x); }
+    bool operator > (const pt &o) const { return y > o.y || (y == o.y && x > o.x); }
 };
 int cmp (lf a, lf b) { return (a + eps < b ? -1 :(b + eps < a ? 1 : 0)); } //double comparator
 
@@ -33,26 +33,26 @@ bool are_perp(pt v, pt w) { return dot(v,w) == 0; }
 int sign(T x) { return (T(0) < x) - (x < T(0)); }
 
 bool in_angle(pt a, pt b, pt c, pt x) { // x inside angle abc (center in a)
-  assert(orient(a,b,c) != 0);
-  if (orient(a,b,c) < 0) swap(b,c);
-  return orient(a,b,x) >= 0 && orient(a,c,x) <= 0;
+    assert(orient(a,b,c) != 0);
+    if (orient(a,b,c) < 0) swap(b,c);
+    return orient(a,b,x) >= 0 && orient(a,c,x) <= 0;
 }
 //angle bwn 2 vectors
 lf angle(pt a, pt b) { return acos(max(-1.0, min(1.0, dot(a,b)/abs(a)/abs(b)))); }
 lf angle(pt a, pt b) { return atan2(cross(a, b), dot(a, b)); }
 /// returns vector to transform points
 pt get_linear_transformation(pt p, pt q, pt r, pt fp, pt fq) {
-  pt pq = q-p, num{cross(pq, fq-fp), dot(pq, fq-fp)};
-  return fp + pt{cross(r-p, num), dot(r-p, num)} / norm(pq);
+    pt pq = q-p, num{cross(pq, fq-fp), dot(pq, fq-fp)};
+    return fp + pt{cross(r-p, num), dot(r-p, num)} / norm(pq);
 }
 bool half(pt p) { /// true if is in (0, 180] (line is x axis)
-  assert(p.x != 0 || p.y != 0); /// the argument of (0,0) is undefined
-  return p.y > 0 || (p.y == 0 && p.x < 0);
+    assert(p.x != 0 || p.y != 0); /// the argument of (0,0) is undefined
+    return p.y > 0 || (p.y == 0 && p.x < 0);
 }
 bool half_from(pt p, pt v = {1, 0}) { //line is v (above v is true)
-  return cross(v,p) < 0 || (cross(v,p) == 0 && dot(v,p) < 0);
+    return cross(v,p) < 0 || (cross(v,p) == 0 && dot(v,p) < 0);
 }
 bool polar_cmp(const pt &a, const pt &b) {//polar sort
-  return make_tuple(half(a), 0) < make_tuple(half(b), cross(a,b));
+    return make_tuple(half(a), 0) < make_tuple(half(b), cross(a,b));
 //   return make_tuple(half(a), 0, sq(a)) < make_tuple(half(b), cross(a, b), sq(b)); // further ones appear later
 }
--- c++/4 - Geometry/halfplanes.cpp
+++ c++/4 - Geometry/halfplanes.cpp
@@ -1,12 +1,12 @@
 struct halfplane{
-	double angle;
+    double angle;
     pt p, pq;
-	halfplane(){}
-	halfplane(pt a, pt b): p(a), pq(b - a) {
+    halfplane(){}
+    halfplane(pt a, pt b): p(a), pq(b - a) {
         angle = atan2(pq.y,pq.x);
     }
-	bool operator < (halfplane b)const{return angle < b.angle;}
-	bool out(pt q){return cross(pq, (q-p)) < -eps;} // checks if p is inside the half plane
+    bool operator < (halfplane b)const{return angle < b.angle;}
+    bool out(pt q){return cross(pq, (q-p)) < -eps;} // checks if p is inside the half plane
 };
 
 const lf inf = 1e100;
@@ -17,7 +17,7 @@ pt inter(halfplane& h1, halfplane& h2){
     return h1.p + (h1.pq * alpha);
 }
 
-
+// intersection of halfplanes
 vector<pt> intersect(vector<halfplane>& b){
     vector<pt> box = { {inf, inf}, {-inf, inf}, {-inf, -inf}, {inf, -inf} };
     for(int i = 0; i < 4; i++){
@@ -28,19 +28,19 @@ vector<pt> intersect(vector<halfplane>& b){
     vector<halfplane> c(n + 10);
     for(int i = 0; i < n; i++){
         while(q < h && b[i].out(inter(c[h], c[h-1]))) h--;
-		while(q < h && b[i].out(inter(c[q], c[q+1]))) q++;
-		c[++h] = b[i];
-		if(q < h && abs(cross(c[h].pq, c[h-1].pq)) < eps){
-			if(dot(c[h].pq, c[h-1].pq) <= 0) return {};
-			h--;
-			if(b[i].out(c[h].p)) c[h] = b[i];
-		}
+        while(q < h && b[i].out(inter(c[q], c[q+1]))) q++;
+        c[++h] = b[i];
+        if(q < h && abs(cross(c[h].pq, c[h-1].pq)) < eps){
+            if(dot(c[h].pq, c[h-1].pq) <= 0) return {};
+            h--;
+            if(b[i].out(c[h].p)) c[h] = b[i];
+        }
     }
     while(q < h-1 && c[q].out(inter(c[h], c[h-1]))) h--;
-	while(q < h-1 && c[h].out(inter(c[q], c[q+1]))) q++;
-	if(h - q <= 1)return {};
-	c[h+1] = c[q];
-	vector<pt> s;
-	for(int i = q; i < h+1; i++) s.pb(inter(c[i], c[i+1]));
-	return s;
+    while(q < h-1 && c[h].out(inter(c[q], c[q+1]))) q++;
+    if(h - q <= 1)return {};
+    c[h+1] = c[q];
+    vector<pt> s;
+    for(int i = q; i < h+1; i++) s.pb(inter(c[i], c[i+1]));
+    return s;
 }
