b07e3b9cb7831102f8a275e6d2fb64feb450a261
Manuel Salazar <manuelsalazar3010@gmail.com>
2024-05-26 15:27:44 -0500

    Add code templates

---
+++ c++/Utilities/Template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastIO ios::sync_with_stdio(0), cin.tie(0)
+#define endl '\n'
+#define pb push_back
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    fastIO;
+    // Tu código...
+    int a, b;
+    cin >> a >> b;
+    double c = a + b;
+    // Para imprimir con 6 decimales
+    // cout << fixed << setprecision(6);
+    cout << c << endl;
+}
