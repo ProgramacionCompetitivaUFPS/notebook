abe05280364857a9e4f703e650ea7e3a9f0269de
Manuel Salazar <manuelsalazar3010@gmail.com>
2024-05-27 21:24:37 -0500

    Comments in spanish

--- c++/Data structures/Fenwick Tree.cpp
+++ c++/Data structures/Fenwick Tree.cpp
@@ -14,12 +14,12 @@ int rsq(int k) {
 }
 
 int rsq(int i, int j) { return rsq(j) - rsq(i-1); }
-// last value <= or < to val
+// Ultimo valor <= o < a val
 int lower_find(int val) {
     int id = 0;
     for (int i = 31-__builtin_clz(N); i >= 0; --i) {
         int nid = id | (1<<i);
-        if (nid <= N && bit[nid] <= val) { // or bit[nid] < val
+        if (nid <= N && bit[nid] <= val) { // o bit[nid] < val
             val -= bit[nid];
             id = nid;
         }
