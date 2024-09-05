ed673a14a99c4e97fff4450c570dcc929f43e967
Manuel Salazar <manuelsalazar3010@gmail.com>
2024-09-04 21:15:13 -0500

    Update dsu on tree

--- c++/Data structures/Dsu on Tree.cpp
+++ c++/Data structures/Dsu on Tree.cpp
@@ -1,45 +1,41 @@
-Tecnica basada en disjoint set, util para responer queries sobre arboles del tipo "cuantos vertices en el subarbol de u cumplen con alguna propiedad" en O(n log(n)) para todas las queries.
+> Util para responder queries del tipo "cuantos vertices en el subárbol de u cumplen con x propiedad" en O(nlogn * k) para todas las queries.
+> Esta implementación permite mantener una única estructura global agregando y quitando información. k es el costo de actualizar esa estructura global.
+> Problema de ejemplo: dado un árbol con los nodos coloreados, las consultas son: cuántos nodos del subárbol de u tienen color igual a c.
 
-const int MX = 1e5+5;
-vector<int> g[MX];
-int sz[MX];
-bool big[MX];
-int cnt[MX];
+const int N = 1e5+5;
+vector<int> g[N];
+int sz[N], big[N]; // big[u] = el hijo de u con mayor tamaño
+int col[N]; // Del problema de ejemplo
+int cnt[N]; // Estructura global del ejemplo
 
-void pre(int u, int p) {
-    sz[u] = 1;
+int pre(int u, int p) {
+    sz[u] = 1; big[u] = -1;
+    int mx = 0;
     for (auto &v : g[u]) {
         if (v != p) {
-            pre(v, u);
-            sz[u] += sz[v];
+            sz[u] += pre(v, u);
+            if (sz[v] > mx) mx = sz[v], big[u] = v;
         }
     }
+    return sz[u];
 }
 
 void upd(int u, int p, int x) {
-    cnt[u] += x;
+    cnt[col[u]] += x; // En el problema de ejemplo k = O(1)
     for (auto &v : g[u]) 
-        if (v != p && !big[v]) 
+        if (v != p && v != big[u]) 
             upd(v, u, x);
 }
 
 void dfs(int u, int p, bool keep) {
-    int mx = -1, id = -1;
     for (auto &v : g[u]) 
-        if (v != p && sz[v] > mx) 
-            mx = sz[v], id = v;
-    for (auto &v : g[u]) 
-        if (v != p && v != id) 
+        if (v != p && v != big[u]) 
             dfs(v, u, false);
-    if (id != -1) {
-        dfs(id, u, true);
-        big[id] = true;
-    }
+    if (big[u] != -1) dfs(big[u], u, true);
+    // Agregar información del subárbol de los hijos de u != a big[u]
     upd(u, p, 1);
-    /*Aqui se responden las queries. cnt[u] es el numero de
-    vertices en el subarbol de u que cumplen la propiedad.*/
-    if (id != -1) 
-        big[id] = false;
-    if (!keep) 
-        upd(u, p, -1);
+    // Aqui se responderían las queries
+    // cnt[c] = cantidad de nodos del subárbol de u con color c
+    big[u] = -1;
+    if (!keep) upd(u, p, -1);  // Quitar información del subárbol de u
 }
