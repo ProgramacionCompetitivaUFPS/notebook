2883cae472dd50684645f606fc13a70c0bdc9f97
Manuel Salazar <manuelsalazar3010@gmail.com>
2024-10-08 00:06:39 -0500

    Better Ternary search

--- c++/Math/Ternary Search.cpp
+++ c++/Math/Ternary Search.cpp
@@ -1,14 +1,15 @@
-Retorna el valor minimo de una funcion entre l y r. Se recomienda usar de 50 a 90 iteraciones.
+Retorna el valor máximo de una función f(x) entre [l, r]
 
-double f(double x) {
-    double y = x; // funcion a evaluar que depende de x
-    return y;
-}
+const double eps = 1e-9;
 
-double ternary_search(double l, double r, int it) {
-    double a = (2.0*l + r)/3.0;
-    double b = (l + 2.0*r)/3.0;
-    if (it == 0) return f(a);
-    if (f(a) < f(b)) return ternary_search(l, b, it-1);
-    return ternary_search(a, r, it-1);
+double ternary_search(double l, double r) {
+    while (r - l > eps) { // O se pueden hacer unas 100 iteraciones
+        double m1 = l + (r - l) / 3;
+        double m2 = r - (r - l) / 3;
+        double f1 = f(m1);
+        double f2 = f(m2);
+        if (f1 < f2) l = m1;
+        else r = m2;
+    }
+    return f(l);
 }
